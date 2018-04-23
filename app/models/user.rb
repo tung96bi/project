7e9e9169c288a4881a9bd6e587dc36d9b7edc20f
tung96bi <maixuantung1996@gmail.com>
2018-04-23 16:35:28 +0700

    chap-7
    	modified:   app/controllers/users_controller.rb
    
    	modified:   app/assets/stylesheets/custom.scss
    
    	modified:   app/controllers/users_controller.rb
    	new file:   app/views/shared/_error_messages.html.erb
    	modified:   app/views/users/new.html.erb
    	modified:   config/locales/en.yml

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   enum gender: [:male, :female]
 
-  validates :email, presence: true, length: {maximum: Setting.max_email},
+  validates :email, presence: true, length: {maximum: Settings.max_email},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: Setting.min_pass}
-  validates :name, presence: true, length: {maximum: Setting.max_name}
+  validates :password, presence: true, length: {minimum: Settings.min_pass}
+  validates :name, presence: true, length: {maximum: Settings.max_name}
   validates :birthday, presence: true
-  validates :gender, inclusion: {in: gender}
+  validates :gender, inclusion: {in: genders}
 
   before_save :email_downcase
 
